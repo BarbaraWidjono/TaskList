90d89cd80ac185fbacb104be10da9a9dbfc92b94
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-20 15:23:25 -0700

    Creating an update method

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create', as: 'create_task'
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
 
 
 end
