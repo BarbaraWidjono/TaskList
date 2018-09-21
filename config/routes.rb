ad9cf4954751b73abc46109073faade957d0e1c7
BarbaraWidjono <barbarawidjono@gmail.com>
2018-09-21 09:46:14 -0700

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create', as: 'create_task'
+  post '/tasks', to: 'tasks#create'
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
